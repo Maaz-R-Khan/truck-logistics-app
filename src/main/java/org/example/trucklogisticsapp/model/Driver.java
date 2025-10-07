1f4e29fe0e0c6f4557cffb953aee7b89e07427d3
Xero <yunusemreoz117@gmail.com>
2025-10-07 10:31:08 -0400

    saving progress

--- src/main/java/org/example/trucklogisticsapp/model/Driver.java
+++ src/main/java/org/example/trucklogisticsapp/model/Driver.java
@@ -1,4 +1,31 @@
 package org.example.trucklogisticsapp.model;
 
+import java.util.UUID;
+
 public class Driver {
+
+    private String id;
+    private String name;
+
+    private String licenseNumber;
+    private String licenseExpiry;
+
+    private String phone;
+    private String Address;
+
+    private String photoPath;
+    private String resumePath;
+
+    private String backgroundNotes;
+
+    private boolean active = true;
+    private String createdAt;
+
+    public Driver() {
+
+    }
+
+    public Driver(String id, String licenseNumber){
+        this.id = UUID.randomUUID().toString();
+    }
 }
